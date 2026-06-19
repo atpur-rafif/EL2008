3cb2783a02b6fecdd9ed8a1fb838c33a176f7be0
atpur-rafif <muhammadatpurrafif@gmail.com>
2026-06-19 20:37:45 +0700

    feat: implement delete

---
+++ src/delete.c
@@ -0,0 +1,22 @@
+#include "delete.h"
+
+void deleteBarang(struct List** list, int id, struct Barang** barang){
+	if(*list == NULL){
+		*barang = NULL;
+		return;
+	}
+
+	struct List* current = *list;
+	if(current->barang->id == id){
+		*barang = current->barang;
+		*list = current->next;
+		return;
+	}
+
+	while(current->next != NULL && current->next->barang->id != id)
+		current = current->next;
+
+	struct List* item = current->next;
+	current->next = item->next;
+	*barang = item->barang;
+}
--- src/delete.h
+++ src/delete.h
@@ -1,8 +1,9 @@
 #ifndef DELETE_H
 #define DELETE_H
 
+#include <stdlib.h>
 #include "struct.h"
 
-void deleteBarang(struct List* list, int id);
+void deleteBarang(struct List** list, int id, struct Barang** barang);
 
 #endif
